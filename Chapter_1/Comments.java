7c184ec4574bc66aa91693491391df327d77e3af
LukeAbendan-UCM <abendanluke@gmail.com>
2025-09-03 23:08:58 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,17 @@
+/**
+ * This is a documentation comment.
+ * Describes the purpose of the class
+ */
+public class Comments {
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+        System.out.println("Java comments");
+
+        /*
+         * This is a multi-line comment.
+         * Useful for longer explanations as it can span multiple lines.
+         */
+        System.out.println("I'm using single-line, multi-line, and documentation comments.");
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,10 @@
+public class FavoriteSong {
+
+    public static void main(String[] args) {
+
+        System.out.println("And I remember first time that I left,");
+        System.out.println("Pain, it flooded the wells of your eyes,");
+        System.out.println("You said you'd never forgive me if I didn't make it back,");
+        System.out.println("But now I'm comin' home for some good lovin'.");
+    }
+}
