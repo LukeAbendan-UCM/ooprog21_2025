0f9cfd2373e419bf09fe21dc5003cb6109a03ab3
LukeAbendan-UCM <abendanluke@gmail.com>
2025-09-29 08:57:40 +0800

    Update DebugPen.java

--- Chapter_4/DebugPen.java
+++ Chapter_4/DebugPen.java
@@ -19,12 +19,4 @@ public class DebugPen {
     public String getPoint() {
         return point;
     }
-
-    public static void main(String[] args) {
-        DebugPen defaultPen = new DebugPen();
-        System.out.println("Default Pen - Color: " + defaultPen.getColor() + ", Point: " + defaultPen.getPoint());
-
-        DebugPen customPen = new DebugPen("blue", "medium");
-        System.out.println("Custom Pen - Color: " + customPen.getColor() + ", Point: " + customPen.getPoint());
-    }
 }
